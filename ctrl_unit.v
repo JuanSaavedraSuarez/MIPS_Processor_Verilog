59a00cf5e631cd5ea760af3d7879c5abb40fb9b3
j s <jlssz@proton.me>
2025-04-20 23:49:02 -0700

    Created ctrl_unit.v module

---
+++ ctrl_unit.v
@@ -0,0 +1,16 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Engineer: Juan Saavedra Suarez
+// 
+// Module Name: ctrl_unit.v
+// Project Name: Verilog MIPS_Processor_Verilog
+// Tool Versions: 
+// Description: Verilog implementation of Control Unit for MIPS Processor
+// Signals
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module ctrl_unit(
+    );
+
+endmodule
